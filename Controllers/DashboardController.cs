dd78d7661c3059371b14a4af0ba9fcbaddac7557
lg_hoaian <luonghoaian2005@gmail.com>
2026-04-05 18:49:42 +0700

    giao dien profile

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseWeb.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var token = HttpContext.Session.GetString("AccessToken");
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            ViewBag.UserFullName = HttpContext.Session.GetString("UserFullName");
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/Auth/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseWeb.Models.ViewModels.Auth
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Full name is required.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+
+        [Display(Name = "I agree to the terms and conditions")]
+        public bool AgreeTerms { get; set; }
+    }
+}
