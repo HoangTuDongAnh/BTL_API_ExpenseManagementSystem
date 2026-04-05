0b33a67cca567bde33a82db5e35c71f38393741c
Harry <hoangnguyen72.dev@gmail.com>
2026-04-05 21:49:20 +0700

    xoa file rac

--- Models/Dtos/Auth/LoginRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExpenseWeb.Models.Dtos.Auth
-{
-    public class LoginRequestDto
-    {
-        public string email { get; set; }
-        public string password { get; set; }
-    }
-}
--- Models/Dtos/Auth/LoginResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExpenseWeb.Models.Dtos.Auth
-{
-    public class LoginResponseDto
-    {
-        public string access_token { get; set; }
-        public string token_type { get; set; }
-        public UserDto user { get; set; }
-    }
-}
